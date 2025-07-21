715f8cfd3d171d61dabdd8424d27efddccfc3531
Huy Trịnh <trinhnhathuy3@gmail.com>
2025-07-21 20:25:31 +0700

    fix deploy vercel

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,16 @@
-export const API_ROOT = "https://endlessly-enabling-husky.ngrok-free.app";
+let apiRoot = "";
+
+console.log("import.meta.env : ", import.meta.env);
+console.log("process.env : ", process.env);
+
+if (process.env.BUILD_MODE === "production") {
+    apiRoot = "https://endlessly-enabling-husky.ngrok-free.app";
+}
+
+if (process.env.BUILD_MODE === "dev") {
+    apiRoot = "https://endlessly-enabling-husky.ngrok-free.app";
+}
+
+console.log("apiRoot", apiRoot);
+
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    define: {
+        "process.env": process.env,
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: [{ find: "~", replacement: "/src" }],
