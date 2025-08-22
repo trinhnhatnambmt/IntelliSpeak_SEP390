dde9883e5a3a955fdeaa30282c84a5afe4b4d22c
Huy Trịnh <trinhnhathuy3@gmail.com>
2025-08-22 21:46:12 +0700

    Language page

--- src/assets/index.js
+++ src/assets/index.js
@@ -36,8 +36,12 @@ import authThumb from "./auth/authThumbnail.jpg";
 import authThumb2 from "./auth/authThumbnail2.jpg";
 import robot from "./robot.png";
 import robot_assistant from "./robot-assistant.png";
+import flagOfEn from "./Flag_of_the_United_Kingdom.png";
+import flagOfVi from "./Flag_of_Vietnam.svg.png";
 
 export {
+    flagOfVi,
+    flagOfEn,
     robot_assistant,
     robot,
     authThumb,
