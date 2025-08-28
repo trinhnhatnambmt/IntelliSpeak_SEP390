bb7abdec0199ad767d8507719205c05d2c7357a6
Huy Trịnh <trinhnhathuy3@gmail.com>
2025-08-28 19:42:58 +0700

    Change domain

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,11 +4,13 @@ let apiRoot = "";
 // console.log("process.env : ", process.env);
 
 if (process.env.BUILD_MODE === "production") {
-  apiRoot = "https://endlessly-enabling-husky.ngrok-free.app";
+    apiRoot =
+        "https://bug-adapting-especially.ngrok-free.app/swagger-ui/index.html";
 }
 
 if (process.env.BUILD_MODE === "dev") {
-  apiRoot = "https://endlessly-enabling-husky.ngrok-free.app";
+    apiRoot =
+        "https://bug-adapting-especially.ngrok-free.app/swagger-ui/index.html";
 }
 
 // console.log("apiRoot", apiRoot);
