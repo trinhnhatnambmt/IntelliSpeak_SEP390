bf3cf6b519cc09f6473034854c2226cf5e48dfb0
Huy Trịnh <trinhnhathuy3@gmail.com>
2025-08-28 19:44:21 +0700

    Change domain

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,13 +4,11 @@ let apiRoot = "";
 // console.log("process.env : ", process.env);
 
 if (process.env.BUILD_MODE === "production") {
-    apiRoot =
-        "https://bug-adapting-especially.ngrok-free.app/swagger-ui/index.html";
+    apiRoot = "https://bug-adapting-especially.ngrok-free.app";
 }
 
 if (process.env.BUILD_MODE === "dev") {
-    apiRoot =
-        "https://bug-adapting-especially.ngrok-free.app/swagger-ui/index.html";
+    apiRoot = "https://bug-adapting-especially.ngrok-free.app";
 }
 
 // console.log("apiRoot", apiRoot);
